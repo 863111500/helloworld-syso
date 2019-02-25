94af7febab6747766988a8114d32d967beff7aa6
chujg <chujingu@163.com>
2019-02-25 10:02:08 +0800

    更新

--- src/main/java/com/jinx/HelloworldSysoApplication.java
+++ src/main/java/com/jinx/HelloworldSysoApplication.java
@@ -2,9 +2,17 @@ package com.jinx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloworldSysoApplication {
+public class HelloworldSysoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloworldSysoApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldSysoApplication.class, args);
